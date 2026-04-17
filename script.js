7effd2f1e7ecb89d1ccc5332d216ce93f99d323a
EdilaneTI <edilane.s.costa@gmail.com>
2026-04-17 18:53:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const formulario = document.querySelector('#meu-formulario');
+
+function validarFormulario(event) {
+    event.preventDefault(); 
+
+    const nome = document.querySelector('#nome').value;
+    const email = document.querySelector('#email').value;
+    const msg = document.querySelector('#msg').value;
+
+    if (nome === "" || email === "" || msg === "") {
+        alert("Por favor, preencha todos os campos antes de enviar.");
+    } else {
+        alert(`Obrigado, ${nome}! Sua mensagem foi enviada com sucesso.`);
+        formulario.reset(); 
+    }
+}
+
+formulario.addEventListener('submit', validarFormulario);
+
+const btnTema = document.querySelector('#btn-tema');
+const body = document.body;
+
+function alternarTema() {
+    body.classList.toggle('dark-theme');
+    
+    // Altera o texto do botão conforme o tema
+    if (body.classList.contains('dark-theme')) {
+        btnTema.innerText = "Claro";
+    } else {
+        btnTema.innerText = "Escuro";
+    }
+}
+
+btnTema.addEventListener('click', alternarTema);
+
+const meusProjetos = [
+    {
+        titulo: "Projeto 01 - Portifólio",
+        descricao: "Aprendendo sobre github e programação web.",
+        link: "https://github.com/EdilaneTI"
+    },
+    {
+        titulo: "Projeto 02 - Simulador de Votação" ,
+        descricao: "Código em portugol, aprendendo programação.",
+        link: "https://github.com/EdilaneTI/Portugol/blob/main/eleicoes"
+    },
+    {
+        titulo: "Projeto 03 - Aulas",
+        descricao: "Códigos aprendendo programação.",
+        link: "https://github.com/EdilaneTI/Aulas"
+    }
+];
+
+function renderizarProjetos() {
+    const container = document.querySelector('.projetos-container');
+    
+    container.innerHTML = "";
+
+    meusProjetos.forEach(projeto => {
+        const cardHTML = `
+            <article class="card-projeto">
+                <h3>${projeto.titulo}</h3>
+                <p>${projeto.descricao}</p>
+                <a href="${projeto.link}" target="_blank">Ver no GitHub</a>
+            </article>
+        `;
+        container.innerHTML += cardHTML;
+    });
+}
+
+renderizarProjetos();
